268f57bfd8fed78fc3c3f2305ead70fdf6a1e92e
DefkalionPap <deucalionpapadopoulos@gmail.com>
2025-03-05 04:50:35 -0800

    Update voCapImporter.cs

--- voCapImporter.cs
+++ voCapImporter.cs
@@ -1,83 +1,27 @@
 using UnityEngine;
-using System.IO;
-using Unity.VisualScripting;
-using UnityEngine.UI;
-
-public class voCapImporter : MonoBehaviour
+public class VoCapImporter : MonoBehaviour
 {
     #region Fields
-    [SerializeField] bool ClosePlugin = false;
-    [SerializeField] GameObject glTFObject = null;
-    [SerializeField] Vector3 position = Vector3.zero;
-    [SerializeField] Quaternion rotation = Quaternion.Euler(-90, 0, 0); 
-    [SerializeField] private bool AddObject = false;
-    private GameObject temp;
-    MeshRenderer meshRenderer;
-    MeshFilter meshFilter;
-    Vector3[] objectVertices;
-    Vector2[] objectUV;
-    int[] objectTriangles;
+    [SerializeField] private GameObject glbObject;
+    [SerializeField] private bool addObject;
     #endregion
     
-    #region Properties
-    public Vector3[] ObjectVertices
-    {
-        get { return objectVertices; }
-        set { objectVertices = value; }
-    }
-    public Vector2[] ObjectUV
-    {
-        get { return objectUV; }
-        set { objectUV = value; }
-    }
-    public int[] ObjectTriangles
-    {
-        get { return objectTriangles; }
-        set { objectTriangles = value; }
-    }
-    #endregion
-
     #region Methods
-    public void Start()
-    {
-        temp = glTFObject;
-    } // Runs at first frame
     public void Update()
     {
-        if (glTFObject != null)       //Make sure it also checks if the file is glTF or glb
-        
+        if (glbObject != null)       //Check if file is glTF or glb
         {
             SpawnObject();
-            UpdateObjectMesh();
         } 
-        DestroyPlugin(ClosePlugin);
-    } // Runs every frame
-    public void DestroyPlugin(bool Close_Plugin)
-    {
-        if (Close_Plugin)
-        {
-            Destroy(gameObject);
-        }
-        
-    } // Destroys plugin*
-   
+    }
     public void SpawnObject()
     {
-        if (AddObject)
+        if (addObject)
         {
-            Instantiate(glTFObject, position, rotation);
-            AddObject = false;
+            //Add components to file, then instantiate
+            Instantiate(glbObject);
+            addObject = false;
         }
     } // Spawns object
-    public void UpdateObjectMesh()
-    {
-        //Update gameObject meshes for every frame https://bit.ly/unitydocs_mesh
-        //GetComponent<MeshFilter>().mesh = glTFMesh;           //Line from documentation
-        // Get vertices, uv, and triangles from glTF
-        //glTFMesh.vertices = objectVertices;
-        //glTFMesh.uv = objectUV;
-        //glTFMesh.triangles = objectTriangles;
-    } // Updates object's mesh and material
-   
     #endregion
 }
