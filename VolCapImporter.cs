3258681d9f56f8f801128e094c03ebabf475659d
DefkalionPap <deucalionpapadopoulos@gmail.com>
2025-03-12 15:02:37 +0200

    Update VolCapImporter.cs

--- VolCapImporter.cs
+++ VolCapImporter.cs
@@ -1,51 +1,75 @@
-using UnityEngine;
-using System.Collections;
-using GLTFast;
-using UnityEditor;
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
-
-public class glTFList : MonoBehaviour
+using System.Linq;
+using UnityEngine;
+using GLTFast;
+using System;
+public class VolCapImporter : MonoBehaviour
 {
-    
     #region Fields
-    GameObject glbObject;
+
     [SerializeField] private float FPS = 30;
     [SerializeField] private bool loop = true;
     bool loaded = false;
     int renderedFrames = 0;
     private float timer = 0;
     float frameDuration;
-    private List<Mesh> volCapMeshes = new List<Mesh>() ;
+    private List<Mesh> volCapMeshes = new List<Mesh>();
     List<Texture> volCapTextures = new List<Texture>();
-    [SerializeField] List<GameObject> glTFObjects;
-    string path;
     int objectIndex = 0;
     int successCount = 0;
     bool over = false;
     bool hasComponents = false;
+    List<string> filePath = new List<string>();
+
     #endregion
 
-    #region Methods
-    
+    public string folder;
+    string path;
+    [SerializeField] List<GameObject> sequence = new List<GameObject>();
+    string temp;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-        gameObject.transform.rotation = Quaternion.Euler( -90, 0, 180 );
-        
-        //Load File
+        #region New Code - Load from folder
+
+        path = Application.dataPath + "/Resources/" + folder;
+        string[] files = Directory.GetFiles(path).Where(s => s.EndsWith(".glb")).ToArray();
+
+        // Debug log
+        for (int i = 0; i < files.Length; i++)
+        {
+           // Debug.Log(files[i]);
+        }
+
+        // Get files
+        for (int i = 0; i < files.Length; i++)
+        {
+            temp = folder + "/" + Path.GetFileNameWithoutExtension(files[i]);
+            //Debug.Log(temp);
+            filePath.Add(files[i]);
+            //Debug.Log(filePath[i]);
+            sequence.Add(Resources.Load<GameObject>(temp));
+        }
+
+        #endregion
+
+        gameObject.transform.rotation = Quaternion.Euler(-90, 0, 180);
         LoadGltfBinaryFromMemory();
+
     }
 
-    public void FixedUpdate() 
+    #region Rest
+
+    
+    public void FixedUpdate()
     {
         if (over)
         {
             timer += Time.fixedDeltaTime;
             frameDuration = 1f / FPS; //AI assisted line
-        
+
             if (timer >= frameDuration && loaded)
             {
                 if (renderedFrames < volCapMeshes.Count) //AI assisted line
@@ -59,94 +83,89 @@ public class glTFList : MonoBehaviour
                 }
             }
 
-            if (loop = true && renderedFrames == volCapMeshes.Count)
+            if (loop == true && renderedFrames == volCapMeshes.Count)
                 renderedFrames = 0;
         }
-        
-        
     }
-    
-    async void LoadGltfBinaryFromMemory() {
+        
+    async void LoadGltfBinaryFromMemory()
+                {
 
-        while (objectIndex < glTFObjects.Count - 1)
-        {
-            glbObject = glTFObjects[objectIndex];
-                    path = Application.dataPath + AssetDatabase.GetAssetPath( glbObject ).Replace( "Assets", "" );                     // Gets asset's absolute path
-                 //   Debug.Log( path );
-                    
-                    byte[] data;
-                    using (StreamReader streamReader = new StreamReader(path))
+                    while (objectIndex < sequence.Count - 1)
                     {
-                        using (MemoryStream memoryStream = new MemoryStream())
+                        
+                        byte[] data;
+                        temp = filePath[objectIndex];
+                        using (StreamReader streamReader = new StreamReader(temp))
                         {
-                            streamReader.BaseStream.CopyTo(memoryStream);
-                            data = memoryStream.ToArray();
+ 
+                            using (MemoryStream memoryStream = new MemoryStream())
+                            {
+                                streamReader.BaseStream.CopyTo(memoryStream);
+                                data = memoryStream.ToArray(); 
+                            }
                         }
-                    }
-                    var gltf = new GltfImport();
-                    bool success = await gltf.LoadGltfBinary(
-                        data,
-                        // The URI of the original data is important for resolving relative URIs within the glTF
-                        new Uri(path)
-                    );
-            
-                    #region Debug
-            
-                   // Debug.Log(success);
-                    if (success)   
-                        loaded = true;
-                    successCount++;
-                    Debug.Log("Video Loaded: " + successCount);
-                    int meshCount;
-                    Mesh[] meshes = gltf.GetMeshes();
-                    meshCount = meshes.Length;
-                   
-            
-                    #endregion
-                    // Show values on console
-            
-                    #region Load Meshes and Textures
-            
-                    //Load meshes
-                    for (int i = 0; i < meshCount; i++)
-                    {
-                        volCapMeshes.Add(meshes[i]);
-                    }
+
+                        var gltf = new GltfImport();
+                        bool success =
+                            await gltf.LoadGltfBinary(data,
+                                new Uri(filePath[
+                                    objectIndex])); //The URI of the original data is important for resolving relative URIs within the glTF
                     
-                    //Load textures
-                    for (int i = 0; i < gltf.TextureCount; i++)
-                    {
-                        volCapTextures.Add(gltf.GetTexture(i));
-                    }
-                    
-                    AddComponents();
-            
-                    #endregion
-                    
-                    if (objectIndex < glTFObjects.Count - 1)
-                        objectIndex++;
-                    if (objectIndex == glTFObjects.Count - 1)
-                        over = true;
-        }
+
+                        #region Debug
         
-    }
+                        // Debug.Log(success);
+                        if (success)
+                            loaded = true;
+                        successCount++;
+                        Debug.Log("Video Loaded: " + successCount);
+                        int meshCount;
+                        Mesh[] meshes = gltf.GetMeshes();
+                        meshCount = meshes.Length;
+        
+                        #endregion
+        
+                        // Show values on console
+        
+                        #region Load Meshes and Textures
+        
+                        //Load meshes
+                        for (int i = 0; i < meshCount; i++)
+                        {
+                            volCapMeshes.Add(meshes[i]);
+                        }
+        
+                        //Load textures
+                        for (int i = 0; i < gltf.TextureCount; i++)
+                        {
+                            volCapTextures.Add(gltf.GetTexture(i));
+                        }
+        
+                        AddComponents();
+        
+                        #endregion
+        
+                        if (objectIndex < sequence.Count - 1)
+                            objectIndex++;
+                        if (objectIndex == sequence.Count - 1)
+                            over = true;
+                    }
+        
+                }
 
     public void AddComponents()
-    {
-        if (hasComponents == false)
         {
-            //Add components
-            gameObject.AddComponent<MeshFilter>();
-            gameObject.AddComponent<MeshRenderer>();
-            gameObject.GetComponent<MeshRenderer>().material.shader = Shader.Find("Universal Render Pipeline/Unlit");
-            hasComponents = true;
+            if (hasComponents == false)
+            {
+                //Add components
+                gameObject.AddComponent<MeshFilter>();
+                gameObject.AddComponent<MeshRenderer>();
+                gameObject.GetComponent<MeshRenderer>().material.shader =
+                    Shader.Find("Universal Render Pipeline/Unlit");
+                hasComponents = true;
+            }
         }
-    }
 
     #endregion
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    
-
-    
-   
-}
+    }
