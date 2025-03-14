3949bd3a135026085605875012048aad3a878273
DefkalionPap <deucalionpapadopoulos@gmail.com>
2025-03-14 07:26:33 -0700

    Update VolumetricCapturePlayer.cs

--- VolumetricCapturePlayer.cs
+++ VolumetricCapturePlayer.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
 public class VolumetricCapturePlayer : MonoBehaviour
@@ -12,23 +14,23 @@ public class VolumetricCapturePlayer : MonoBehaviour
     int renderedFrames = 0;
     private float timer = 0;
     float frameDuration;
-    [SerializeField] List<Mesh> volCapMeshes = new List<Mesh>();
-    [SerializeField] List<Texture> volCapTextures = new List<Texture>();
+    [SerializeField] List<Mesh> meshes = new List<Mesh>();
+    [SerializeField] List<Texture> textures = new List<Texture>();
     bool loaded = true;
     
-    public List<GameObject> Sequence { get { return sequence; } set { sequence = value; } }
-    public List<Texture> SequenceTextures { get { return volCapTextures; } set { volCapTextures = value; } }
-    public List<Mesh> SequenceMeshes { get { return volCapMeshes; } set { volCapMeshes = value; } }
-    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public void Awake()
+    public void Start()
     {
+        sequence = gameObject.GetComponent<LoadData>().Sequence;
+        meshes = gameObject.GetComponent<LoadMeshesAndTextures>().Meshes;
+        textures = gameObject.GetComponent<LoadMeshesAndTextures>().Textures;
         gameObject.GetComponent<MeshRenderer>().material.shader = Shader.Find("Universal Render Pipeline/Unlit");
         gameObject.transform.rotation = Quaternion.Euler(-90, 0, 270);
     }
 
     public void FixedUpdate()
     {
+        over = gameObject.GetComponent<LoadMeshesAndTextures>().Over;
         if (over)
         {
             timer += Time.fixedDeltaTime;
@@ -36,21 +38,19 @@ public class VolumetricCapturePlayer : MonoBehaviour
 
             if (timer >= frameDuration && loaded)
             {
-                if (renderedFrames < volCapMeshes.Count) //AI assisted line
+                if (renderedFrames < meshes.Count) //AI assisted line
                 {
                     MeshFilter meshFilter = gameObject.GetComponent<MeshFilter>();
                     Renderer renderer = gameObject.GetComponent<Renderer>();
-                    meshFilter.mesh = volCapMeshes[renderedFrames];
-                    renderer.material.mainTexture = volCapTextures[renderedFrames];
+                    meshFilter.mesh = meshes[renderedFrames];
+                    renderer.material.mainTexture = textures[renderedFrames];
                     renderedFrames++;
                     timer -= frameDuration; // Reset timer and Keep the overflow to carry forward the remainder - AI assisted line
                 }
             }
 
-            if (loop == true && renderedFrames == volCapMeshes.Count)
+            if (loop == true && renderedFrames == meshes.Count)
                 renderedFrames = 0;
         }
     }
-    
-    
 }
