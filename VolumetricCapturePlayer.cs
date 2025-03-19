0011ad9237b0756a7905b59a93b19cae5d026010
DefkalionPap <deucalionpapadopoulos@gmail.com>
2025-03-19 03:47:11 -0700

    Successfully plays meshes and textures

--- VolumetricCapturePlayer.cs
+++ VolumetricCapturePlayer.cs
@@ -8,8 +8,7 @@ public class VolumetricCapturePlayer : MonoBehaviour
 {
 
     [SerializeField] int FPS = 30;
-    [SerializeField] bool loop = false;
-    [SerializeField] List<GameObject> sequence = new List<GameObject>();
+    [SerializeField] bool loop = true;
     bool over = true;
     int renderedFrames = 0;
     private float timer = 0;
@@ -17,31 +16,35 @@ public class VolumetricCapturePlayer : MonoBehaviour
     [SerializeField] List<Mesh> meshes = new List<Mesh>();
     [SerializeField] List<Texture> textures = new List<Texture>();
     bool loaded = true;
+    private MeshFilter meshFilter;
+    private Renderer renderer;
+    
+    public List<Mesh> Meshes { get { return meshes; } set { meshes = value; } }
+    public List<Texture> Textures { get { return textures; } set { textures = value; } }
+    public bool Over { get { return over; } set { over = value; } }
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Start()
     {
-        sequence = gameObject.GetComponent<LoadData>().Sequence;
-        meshes = gameObject.GetComponent<LoadMeshesAndTextures>().Meshes;
-        textures = gameObject.GetComponent<LoadMeshesAndTextures>().Textures;
-        gameObject.GetComponent<MeshRenderer>().material.shader = Shader.Find("Universal Render Pipeline/Unlit");
+        //gameObject.GetComponent<MeshRenderer>().material.shader = Shader.Find("Universal Render Pipeline/Unlit");
         gameObject.transform.rotation = Quaternion.Euler(-90, 0, 270);
+        meshFilter = gameObject.GetComponent<MeshFilter>();
+        renderer = gameObject.GetComponent<Renderer>();
     }
 
     public void FixedUpdate()
     {
-        over = gameObject.GetComponent<LoadMeshesAndTextures>().Over;
         if (over)
         {
             timer += Time.fixedDeltaTime;
             frameDuration = 1f / FPS; //AI assisted line
+            
 
             if (timer >= frameDuration && loaded)
             {
                 if (renderedFrames < meshes.Count) //AI assisted line
                 {
-                    MeshFilter meshFilter = gameObject.GetComponent<MeshFilter>();
-                    Renderer renderer = gameObject.GetComponent<Renderer>();
+                    
                     meshFilter.mesh = meshes[renderedFrames];
                     renderer.material.mainTexture = textures[renderedFrames];
                     renderedFrames++;
