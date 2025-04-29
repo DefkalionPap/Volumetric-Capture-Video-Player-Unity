50657820853dcc9a87429cf635db0adfff58dffd
DefkalionPap <deucalionpapadopoulos@gmail.com>
2025-04-29 10:32:49 +0300

    Added if UNITY_EDITOR

--- Runtime/Scripts/MenuItemScript.cs
+++ Runtime/Scripts/MenuItemScript.cs
@@ -3,19 +3,18 @@ using UnityEngine;
 
 public class MenuTest : MonoBehaviour
 {
-    
-    [MenuItem("Volumetric Capture Video Player / Volumetric Capture Video", false, 10)]
-    static void CreateVolCapVideo(MenuCommand menuCommand)
-    {
-        GameObject video = new GameObject("Volumetric Capture Video");
-        video.AddComponent<VolumetricCaptureVideoPlayer>();
-        video.AddComponent<MeshFilter>();
-        video.AddComponent<MeshRenderer>();
-        video.GetComponent<MeshRenderer>().material = new Material(Shader.Find("glTF/Unlit"));
-        // Ensure it gets reparented if this was a context click (otherwise does nothing)
-        GameObjectUtility.SetParentAndAlign(video, menuCommand.context as GameObject);
-        // Register the creation in the undo system
-        Undo.RegisterCreatedObjectUndo(video, "Create " + video.name);
-        Selection.activeObject = video;
-    }
+    #if UNITY_EDITOR 
+        [MenuItem("Volumetric Capture Video Player / Volumetric Capture Video", false, 10)]
+        static void CreateVolCapVideo(MenuCommand menuCommand)
+        {
+            GameObject video = new GameObject("Volumetric Capture Video");
+            video.AddComponent<VolumetricCaptureVideoPlayer>();
+            video.AddComponent<MeshFilter>();
+            video.AddComponent<MeshRenderer>();
+            video.GetComponent<MeshRenderer>().material = new Material(Shader.Find("glTF/Unlit"));
+            // Register the creation in the undo system
+            Undo.RegisterCreatedObjectUndo(video, "Create " + video.name);
+            Selection.activeObject = video;
+        }
+    #endif
 }
